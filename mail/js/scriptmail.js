3cdcdcd5922239e7caa8be34ec2616e918a73fec
RobyLedda94 <robyledda01@yahoo.it>
2024-06-28 15:48:04 +0200

    [UPD] ho chiesto all'utente la sua email e ho creato l'array contenente una lista di email

---
+++ mail/js/scriptmail.js
@@ -0,0 +1,14 @@
+// Messaggio alrt di prova per verificare che il file sia linkato correttamente
+// alert("Javascript inserisci la tua mail");
+
+
+// chiediamo all'utente la sua email
+
+let userEmail = prompt('Inserisci la tua email');
+console.log(userEmail);
+
+
+// creiamo la struttura dati che contiene la lista delle email (Array)
+
+const Email = ['robyledda@pippo.it', 'alex@ciao.it', 'antoledda@kita.it', 'gianlu@niki.it'];
+console.log(Email);
