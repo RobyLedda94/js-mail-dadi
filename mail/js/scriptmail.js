d89ddbcd5cd0aa408802efa45b357d0726d84c81
RobyLedda94 <robyledda01@yahoo.it>
2024-06-28 16:03:55 +0200

    [UPD] aggionta condizionale true

--- mail/js/scriptmail.js
+++ mail/js/scriptmail.js
@@ -10,5 +10,20 @@ console.log(userEmail);
 
 // creiamo la struttura dati che contiene la lista delle email (Array)
 
-const Email = ['robyledda@pippo.it', 'alex@ciao.it', 'antoledda@kita.it', 'gianlu@niki.it'];
-console.log(Email);
+const email = ['robyledda@pippo.it', 'alex@ciao.it', 'antoledda@kita.it', 'gianlu@niki.it'];
+console.log(email);
+
+
+// creiamo una variabile di controllo per stabilire che la mail dell'utente sia autorizzata
+
+let found_email = false;
+
+for (let i = 0; i < email.length; i++){
+    if (email[i] === 'robyledda@pippo.it'){
+        found_email = true;
+    }
+}
+
+if (found_email === true) {
+    console.log('La tua email e valida');
+}
