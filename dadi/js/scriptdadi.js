4b0174be81281887ba9cc01b67477cb9c20a24bd
RobyLedda94 <robyledda01@yahoo.it>
2024-06-28 17:29:34 +0200

    [UPD] ho aggiunto la condizionale che stabilisce chi ha vinto

--- dadi/js/scriptdadi.js
+++ dadi/js/scriptdadi.js
@@ -13,3 +13,32 @@
 
 // let userNumber = prompt('Inserisci un numero da 1 a 6');
 // console.log(userNumber);
+
+
+
+
+
+
+// variabile assegnazione numero per l'utente a random
+
+let Usernumber = Math.floor(Math.random() * 6) + 1;
+console.log(Usernumber, 'Numero Giocatore');
+
+
+// variabile assegnazione numero per il computer a random
+let Computernumber = Math.floor(Math.random() * 6) + 1;
+console.log(Computernumber, 'Numero Computer');
+
+
+// creo la condizione che stabilisce la vittoria
+
+if (Usernumber > Computernumber){
+    console.log('Vince il giocatore')
+
+} else if (Usernumber < Computernumber){
+    console.log('Vince il Computer')
+
+} else {
+    console.log('Pareggio')
+}
+
